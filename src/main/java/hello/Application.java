049d8fd55d6cf35216f3ca656120eb9c38dab3d9
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-01-20 22:23:43 +0000

    fds
    sdf

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -3,9 +3,10 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
-
+@EnableResourceServer
 @SpringBootApplication
 public class Application {
 
