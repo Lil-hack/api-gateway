fbe35bd642eccbaa8700997066b3990d9d87bb5e
Zikboy <43441041+akashkinKV@users.noreply.github.com>
2019-02-09 16:54:41 +0000

    456
    456

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
-@EnableResourceServer
+
 @SpringBootApplication
 public class Application {
 
